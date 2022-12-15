15b5894e6558672ffc843c24ecf56640d550d42f
Edgardo Vaca <edgardovac2298@gmail.com>
2022-12-14 23:26:59 -0500

    backend in main

--- backend/src/project/project.controller.ts
+++ backend/src/project/project.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseInterceptors,
+  UploadedFiles,
+} from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { Project } from './schemas/project.schema';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import path, { extname } from 'path';
 
 @Controller('project')
 export class ProjectController {
@@ -10,6 +20,48 @@ export class ProjectController {
   @Post()
   create(@Body() createProjectDto: CreateProjectDto) {
     return this.projectService.create(createProjectDto);
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FilesInterceptor('files', 5, {
+      storage: diskStorage({
+        destination: '../frontend/src/assets/project',
+        filename(req, file, callback) {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          const filename = `${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+      limits: {
+        fileSize: 5000000,
+      },
+    }),
+  )
+  async uploadFile(@UploadedFiles() files: Array<Express.Multer.File>) {
+    if (files.length < 6) {
+      for (const element of files) {
+        if (element.mimetype === 'application/pdf' && element.size < 5000000) {
+          this.projectService.uploadFile(element.originalname, element.buffer);
+        } else {
+          return {
+            msg: 'Solo se permiten archivos pdf, y menores a 5 mb',
+            statusCode: 400,
+          };
+        }
+      }
+      return {
+        msg: 'Los archivos se han sido subido exitosamente',
+        statusCode: 201,
+      };
+    } else {
+      return {
+        msg: 'Cantidad de archivos superior al limite.',
+        statusCode: 400,
+      };
+    }
   }
 
   @Get()
--- backend/src/project/project.service.ts
+++ backend/src/project/project.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateProjectDto } from './dto/create-project.dto';
@@ -13,6 +13,19 @@ export class ProjectService {
   async create(createProjectDto: CreateProjectDto) {
     const createdProject = await this.projectModel.create(createProjectDto);
     return createdProject;
+  }
+
+  async uploadFile(fileName: string, dataBuffer: Buffer) {
+    try {
+      console.log(fileName);
+      console.log(dataBuffer);
+      return {
+        fileName: fileName,
+        dataBuffer: dataBuffer,
+      };
+    } catch (err) {
+      throw new InternalServerErrorException(err);
+    }
   }
 
   async findAll(): Promise<Project[]> {
